28000f69ba7bed5a0a8194db2d5cc6c2825d3bf5
Ayrton <ayrtton.vinicius@gmail.com>
2023-08-31 17:53:57 -0300

    Display the image

--- src/components/RandomPokemon.jsx
+++ src/components/RandomPokemon.jsx
@@ -1,7 +1,13 @@
 function RandomPokemon() {
     let randomValue = Math.floor(Math.random() * 151) + 1;
 
-    return <h1>Pokemon #{randomValue}</h1>;
+    return (
+        <div>
+            <h1>Pokemon #{randomValue}</h1>
+            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" 
+                + randomValue + ".png"} alt="Pokemon Image" />
+        </div>
+    );
 }
 
 export default RandomPokemon;
