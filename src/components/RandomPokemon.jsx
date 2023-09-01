a2b74ae3d3acaa290004d40c4b7d253337682522
Ayrton <ayrtton.vinicius@gmail.com>
2023-09-01 17:41:05 -0300

    Style the component

--- src/components/RandomPokemon.jsx
+++ src/components/RandomPokemon.jsx
@@ -1,8 +1,10 @@
+import "../assets/RandomPokemon.css";
+
 function RandomPokemon() {
     let randomValue = Math.floor(Math.random() * 151) + 1;
 
     return (
-        <div>
+        <div className="container">
             <h1>Pokemon #{randomValue}</h1>
             <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" 
                 + randomValue + ".png"} alt="Pokemon Image" />
