06345d34448b222f737f03ed86a4cedcfff4b68d
Ayrton <ayrtton.vinicius@gmail.com>
2023-09-03 10:28:00 -0300

    Improve the code

--- src/components/RandomPokemon.jsx
+++ src/components/RandomPokemon.jsx
@@ -2,12 +2,12 @@ import "../assets/RandomPokemon.css";
 
 function RandomPokemon() {
     let randomValue = Math.floor(Math.random() * 151) + 1;
+    let imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomValue}.png`
 
     return (
-        <div className="container">
+        <div className="RandomPokemon">
             <h1>Pokemon #{randomValue}</h1>
-            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" 
-                + randomValue + ".png"} alt="Pokemon Image" />
+            <img src={imageUrl} alt="Pokemon Image" />
         </div>
     );
 }
